43c481adfdfa60c4440d0734c1e286d803efa8e8
David González Fernández <d.avid.g.f.2@gmail.com>
2022-03-29 00:11:24 +0200

    Update Game.py
    Implement the code for the person player

--- Game.py
+++ Game.py
@@ -3,20 +3,54 @@ from Tile import Tile
 from Piece import Piece
 from random import choice
 
-def get_person_move(board: Board) -> tuple[Piece, Tile]:
+CHARACTERS = "1234567890ABCDEFGHJKLMNPQRSTUVWXYZ"
+CHARACTERS_NO_ZERO = "123456789ABCDEFGHJKLMNPQRSTUVWXYZ"
+
+def get_person_move(board: Board) -> tuple[Tile, Tile]:
     # Select piece from all it pieces available
-    piece_to_move: Piece = None         #TODO: implement
+    tile_origin: Tile = ask_person_for_piece(board)
 
     # Select the destination tile for the selected piece
-    tile_destination: Tile = None       #TODO: implement
+    tile_destination: Tile = ask_person_for_tile_destination(board, tile_origin)
 
-    return (piece_to_move, tile_destination)
+    return (tile_origin, tile_destination)
+
+def ask_person_for_piece(board: Board) -> Tile:
+    tiles_with_movable_pieces: list[Tile] = list(filter(
+        lambda t: any(board.get_all_possible_tiles_to_move(t)), board.get_person_tiles()
+    ))
+    board.print_board(tiles_with_movable_pieces, "".join(CHARACTERS))
+
+    while True:
+        n = input(f"Select a piece ({CHARACTERS[0]} - {CHARACTERS[len(tiles_with_movable_pieces)-1]})").strip()
+        if n in CHARACTERS[ : len(tiles_with_movable_pieces)]:
+            selected_tile = tiles_with_movable_pieces[CHARACTERS.index(n)]
+            assert selected_tile.get_piece().is_person_piece()
+            return selected_tile
+
+def ask_person_for_tile_destination(board: Board, tile_origin: Tile) -> Tile:
+    assert board is not None
+    assert isinstance(tile_origin, Tile)
+    assert tile_origin.get_piece().is_person_piece()
+
+    available_tile_destinations: list[Tile] = [tile for tile in board.get_all_possible_tiles_to_move(tile_origin)]
+
+    board.print_board(available_tile_destinations, CHARACTERS_NO_ZERO)
+
+    while True:
+        n = input(f"Select a tile to move to ({CHARACTERS_NO_ZERO[0]} - {CHARACTERS_NO_ZERO[len(available_tile_destinations)-1]})").strip()
+        if n in CHARACTERS_NO_ZERO[ : 1+len(available_tile_destinations)]:
+            destination_tile = available_tile_destinations[ CHARACTERS_NO_ZERO.index(n) ]
+            assert destination_tile is not None
+            assert isinstance(destination_tile, Tile)
+            assert destination_tile.is_empty()
+            return destination_tile
 
 
-def get_computer_move(board: Board) -> tuple[Piece, Tile]:
-    piece_to_move, tile_destination = None, None    #TODO: implement
+def get_computer_move(board: Board) -> tuple[Tile, Tile]:
+    tile_origin, tile_destination = None, None    #TODO: implement
 
-    return (piece_to_move, tile_destination)
+    return (tile_origin, tile_destination)
 
 
 if __name__ == "__main__":
@@ -31,15 +65,16 @@ if __name__ == "__main__":
     
     while not board.has_game_ended():
         if is_person_turn:
-            piece_to_move, tile_destination = get_person_move(board)
-            assert piece_to_move.is_person_piece()
-            assert tile_destination.is_empty()
+            tile_origin, tile_destination = get_person_move(board)
+            assert tile_origin.get_piece().is_person_piece()
         else:
-            piece_to_move, tile_destination = get_computer_move(board)
-            assert piece_to_move.is_computer_piece()
-            assert tile_destination.is_empty()
+            tile_origin, tile_destination = get_computer_move(board)
+            assert tile_origin.is_computer_piece()
 
-        board.move_piece_to_tile(piece_to_move, tile_destination)
+        assert tile_destination.is_empty()
+        board.move_piece_to_tile(tile_origin, tile_destination)
+        board.print_board()
+        input()
         is_person_turn = not is_person_turn
     
     if board.has_person_won():
