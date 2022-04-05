4b31c93af4f02fe676f0e097c172d004c7134ff2
David González Fernández <d.avid.g.f.2@gmail.com>
2022-04-05 16:10:29 +0200

    Final version
    Ask if the heuristic function is going to be used

--- Game.py
+++ Game.py
@@ -11,9 +11,9 @@ def create_players() -> tuple[Player_Computer|Player_Person, Player_Computer|Pla
     while player1 is None:
         res = input("Is player1 a person? (y/n): ").strip().lower()
         if res == "y"  or  res == "yes":
-            player1 = Player_Person("player1")
+            player1 = Player_Person("Player1")
         elif res == "n"  or  res == "no":
-            player1 = Player_Computer("player1")
+            player1 = Player_Computer("Player1")
     
     print()
 
@@ -21,7 +21,7 @@ def create_players() -> tuple[Player_Computer|Player_Person, Player_Computer|Pla
     while player2 is None:
         res = input("Is player2 a person? (y/n): ").strip().lower()
         if res == "y"  or  res == "yes":
-            player2 = Player_Person("player2")
+            player2 = Player_Person("Player2")
         elif res == "n"  or  res == "no":
             player2 = Player_Computer("player2")
 
@@ -37,18 +37,29 @@ def get_heuristic(b: Board, turn1: bool):
 
     return h1
 
+def ask_for_heuristic() -> bool:
+    print()
+    while True:
+        res = input("Do you want the computer to use an heuristic? (y/n): ").strip().lower()
+        if res == "y"  or  res == "yes":
+            return True
+        elif res == "n"  or  res == "no":
+            return False
+
 # -----------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     board = Board()
     players = create_players()
+    use_heuristic = ask_for_heuristic()
 
     current_player_index = choice([0, 1])
+    print("\n----------------------------------------------------")
     print(f"{players[current_player_index].get_name()} starts\n")
 
     while not board.has_game_ended():
         player = players[current_player_index]
-        tile_origin, tile_destination = player.get_move(board, get_heuristic(board, player.is_player1()))
+        tile_origin, tile_destination = player.get_move(board, get_heuristic(board, player.is_player1()) if use_heuristic else lambda x, y: True)
         board.move_piece_to_tile(tile_origin, tile_destination)
         board.print_board()
         current_player_index = (current_player_index+1) % len(players)
